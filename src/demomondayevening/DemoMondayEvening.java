ae4d27f604bf94b42074c7241b4699c6e8711161
Nagma <99428751+NagmaKhattar@users.noreply.github.com>
2024-09-16 17:12:48 -0400

    Update DemoMondayEvening.java

--- src/demomondayevening/DemoMondayEvening.java
+++ src/demomondayevening/DemoMondayEvening.java
@@ -20,6 +20,7 @@ public class DemoMondayEvening {
     public static void main(String[] args) 
     {
         // TODO code application logic here
+        //showing fetch and merge
         //making achange on remote
         System.out.println("Hello");
         //gmhdsjkadsl
